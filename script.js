717ea7286efbb806bfa2794a8ef475fe0baa3b3c
fpdab <155249090+fpdab@users.noreply.github.com>
2024-02-02 17:54:12 +0100

    added four math operation functions

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let butNums = document.querySelectorAll("#main-buttons #butNum");
-let butOps = document.querySelectorAll("#main-buttons #butOp");
-let screenNum = document.querySelector("#screen-number");
-let screenOp = document.querySelector("#screen-operand");
-let operandFlag = false;
+const butNums = document.querySelectorAll("#main-buttons #butNum");
+const butOps = document.querySelectorAll("#main-buttons #butOp");
+const screenNum = document.querySelector("#screen-number");
+const screenOp = document.querySelector("#screen-operand");
+const clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", () => {
+  screenNum.textContent = "";
+  screenOp.textContent = "";
+});
+let operandFlag = false; //allows clearing number-screen after inserting operand
+let numberFlag = false; //prevents of inserting operand if there is no number inserted
 
 butNums.forEach((button) =>
   button.addEventListener("click", () => insertNum(button.textContent))
@@ -15,9 +21,30 @@ function insertNum(str) {
   if (operandFlag === true) screenNum.textContent = "";
   screenNum.textContent += str;
   operandFlag = false;
+  numberFlag = true;
 }
 
 function insertOp(str) {
-  operandFlag = true;
-  screenOp.textContent = str;
+  if (numberFlag === true) {
+    screenOp.textContent = str;
+    operandFlag = true;
+    numberFlag = false;
+  }
+}
+
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  if (b !== 0) {
+    return a / b;
+  } else {
+    return "error";
+  }
 }
